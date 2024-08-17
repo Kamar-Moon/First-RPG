5aa43637e7af5a37e575228bd7ec3856d7284c1e
Kamar-Moon <sapphirewolf987@outlook.com>
2024-08-17 16:57:46 +1000

    Created the GameItem, Weapon and ItemFactory classes.

---
+++ Engine/Factories/ItemFactory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class ItemFactory
+    {
+        private static List<GameItem> _standardGameItems;
+
+        static ItemFactory()
+        {
+            _standardGameItems = new List<GameItem>(); //new empty List of type GameItem
+
+            _standardGameItems.Add(new Weapon(1001, "Pointy Stick", 1, 1, 2));
+            _standardGameItems.Add(new Weapon(1002, "Rusty Sword", 5, 1, 3));
+        }
+
+        //Create Copy of item
+        public static GameItem CreateGameItem(int itemTypeID)
+        {
+            //on list varible use LINQ to find first item that matches the itemType ID that matches if not found return default null
+            GameItem StandardItem = _standardGameItems.FirstOrDefault(item => item.ItemTypeID == itemTypeID);
+
+            if (StandardItem != null)
+            {
+                return StandardItem.Clone();
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon :GameItem
+    {
+        public int MinDamage { get; set; }
+        public int MaxDamage { get; set; }
+        public Weapon(int itemTypeID, string name, int price, int minDamage, int maxDamge) 
+            : base(itemTypeID, name, price) //pass in the parameters to this class, they get sent to the base/parent class were they will be set.
+        {
+            MinDamage = minDamage;
+            MaxDamage = MaxDamage;
+        }
+
+
+        public new Weapon Clone() //Override GameItem function of same name with new
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinDamage, MaxDamage);
+        }
+    }
+}
