db159ec8d6a19deb7d22adc13d034d60c28a8b66
Kamar-Moon <sapphirewolf987@outlook.com>
2024-08-20 17:50:24 +1000

    New Monster class added New MonsterFactory class added new RandomNumberGenerator Class added
    new items added to ItemFactory - the monsters loot

---
+++ Engine/Factories/MonsterFactory.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factories
+{
+    public static class MonsterFactory
+    {
+        public static Monster GetMonster(int monsterID)
+        {
+            switch (monsterID)
+            {
+                case 1:
+                    Monster snake =
+                        new Monster("snake", "Snake.png", 4, 4, 5, 1);
+                    AddLootItem(snake, 9001, 25); // 25% chance that snake will drop snake fang item
+                    AddLootItem(snake, 9002, 75);
+
+                    return snake;
+
+                case 2:
+                    Monster rat =
+                        new Monster("Rat", "Rat.png", 5, 5, 5, 1);
+                    AddLootItem(rat, 9003, 25);
+                    AddLootItem(rat, 9004, 75);
+
+                    return rat;
+
+                case 3:
+                    Monster giantSpider =
+                        new Monster("Giant Spider", "GiantSpider.png", 10, 10, 10, 3);
+                    AddLootItem(giantSpider, 9005, 25);
+                    AddLootItem(giantSpider, 9006, 75);
+
+                    return giantSpider;
+
+                default:
+                    throw new ArgumentException(string.Format("MonsterTYpe '{0}' does not exist", monsterID));
+
+            }
+        }
+
+        private static void AddLootItem(Monster monster, int itemID, int percentage)
+        {
+            if(RandomNumberGenerator.NumberBetween(1,100) <= percentage) // if rand number is less than percentage the add
+                // a new item to the monster's inventory. we pass in the itemID and the amount we want to add
+            {
+                monster.Inventory.Add(new ItemQuantity(itemID, 1));
+            }
+        }
+    }
+}
---
+++ Engine/Models/Monster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+
+
+namespace Engine.Models
+{
+    public class Monster : BaseNotificationClass //notify UI of hit points changing
+    {
+        private int _hitPoints;
+
+        public string Name { get; set; }
+        public string ImageName {get; set;}
+        public int MaximumHitPoints { get; private set; }
+        public int HitPoints
+        {
+            get { return _hitPoints; }
+            private set 
+            {
+                _hitPoints = value;
+                OnPropertyChanged(nameof(HitPoints));
+            }
+        }
+
+        public int RewardExperiencePoints { get; private set; }
+        public int RewardGold { get; private set; }
+
+        public ObservableCollection<ItemQuantity> Inventory { get; set; } // loot items from monster
+
+        public Monster(string name, string imageName,
+            int maximumHitPoints, int hitPoints,
+            int rewardExperiencePoints, int rewardGold)
+        {
+            Name = name;
+            ImageName = string.Format("pack://application:,,,/Engine;component/Images/Monsters/{0}", imageName);
+            MaximumHitPoints = maximumHitPoints;
+            HitPoints = hitPoints;
+            RewardExperiencePoints = rewardExperiencePoints;
+            RewardGold = rewardGold;
+
+            Inventory = new ObservableCollection<ItemQuantity>();
+        }
+    }
+}
